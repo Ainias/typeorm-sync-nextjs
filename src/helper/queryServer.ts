e9c6e08b1561c69363d627169674d09ba39bb3cf
silas <silas@silas.link>
2023-03-14 18:24:03 +0100

    new version

--- src/helper/queryServer.ts
+++ src/helper/queryServer.ts
@@ -1,4 +1,4 @@
-import { SyncModel } from '@ainias42/typeorm-sync';
+import { Database, SyncHelper, SyncModel } from '@ainias42/typeorm-sync';
 import { FindManyOptions } from 'typeorm';
 
 export async function queryServer<Model extends typeof SyncModel>(
@@ -19,4 +19,21 @@ export async function queryServer<Model extends typeof SyncModel>(
         return undefined;
     }
     return [];
+
+    // const db = await Database.waitForInstance();
+    // const modelId = Database.getModelIdFor(model);
+    // const result = await db.queryServer(modelId, undefined, query);
+    //
+    // if (result.success === true) {
+    //     const modelContainer = SyncHelper.convertToModelContainer(result.syncContainer);
+    //     const entities = Object.values(modelContainer[modelId]) as InstanceType<Model>[];
+    //     if (singleInstance) {
+    //         if (entities.length > 0) {
+    //             return entities[0];
+    //         }
+    //         return null;
+    //     }
+    //     return entities;
+    // }
+    // throw new Error(result.error.message);
 }
